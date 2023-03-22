93edb32f44b4f4ca1e2d3016caef370761f668b5
gcruzBwise <gcruz28@wgu.edu>
2023-03-22 00:53:33 +0000

    Complete

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class InsertPosition {
 
@@ -15,6 +15,20 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+    int left = 0;
+    int right = nums.length -1;
+
+    while(left <= right ){
+    int middle = left + (right - left)/2;
+    
+    if (nums[middle] == target){
+        return middle;
+    }else if(nums[middle]<target){
+        left = middle +1;
+    }else{
+        right = middle -1;
     }
+}
+return left;
+}
 }
